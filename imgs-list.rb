d8191130e97c66ea5df8ce61258af419759e0ef2
i-tsvetkov <i.tsvetkov.4@gmail.com>
2016-04-10 18:08:23 +0300

    added generated preview

---
+++ imgs-list.rb
@@ -0,0 +1,13 @@
+$imgs = [{ src: "preview-pics/brown-mouse-1452531959vRn.jpg", w: 1440, h: 1920 },
+	{ src: "preview-pics/chipmunk-crouching.jpg", w: 1920, h: 1280 },
+	{ src: "preview-pics/hummingbird-in-the-wild.jpg", w: 1920, h: 1276 },
+	{ src: "preview-pics/kangaroo.jpg", w: 853, h: 1280 },
+	{ src: "preview-pics/lioness-11294081708nT4.jpg", w: 853, h: 1280 },
+	{ src: "preview-pics/panda-rouge-petit-panda.jpg", w: 1272, h: 1920 },
+	{ src: "preview-pics/pigeon-1364561001yVr.jpg", w: 1920, h: 1271 },
+	{ src: "preview-pics/rabbit-1381256083gCi.jpg", w: 1920, h: 1271 },
+	{ src: "preview-pics/victoria-crowned-pigeon-871299334899zrj.jpg", w: 853, h: 1280 },
+	{ src: "preview-pics/vogels-1444987326GcL.jpg", w: 1920, h: 1275 },
+	{ src: "preview-pics/wulf.jpg", w: 1920, h: 1275 },
+	{ src: "preview-pics/zebra-1403093847jNA.jpg", w: 1920, h: 1432 }
+]
