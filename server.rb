be8a4773eb34766c9f25610f5b3fab211673f868
i-tsvetkov <i.tsvetkov.4@gmail.com>
2016-04-18 00:27:46 +0300

    enhanced preview

--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 require 'webrick'
 
 root = File.expand_path '.'
-server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root, MimeTypes: { 'rhtml' => 'text/html' }
+server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root, MimeTypes: { 'rhtml' => 'text/html', 'jpg' => 'image/jpeg' }
 
 WEBrick::HTTPServlet::ERBHandler.new(server, root)
 
